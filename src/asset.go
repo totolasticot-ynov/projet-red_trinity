163d9db20e920027efe1dc75044329e6ccf47a45
dhthomas <thomas.dhordain@ynov.com>
2025-09-17 13:28:04 +0200

    action des rounds

--- src/asset.go
+++ src/asset.go
@@ -69,11 +69,11 @@ func init() {
 	bgGame_dojo, _, _ = ebitenutil.NewImageFromFile("../images/background/level1.png")
 	bgGame_mall, _, _ = ebitenutil.NewImageFromFile("../images/background/level2.png")
 	bgGame_place, _, _ = ebitenutil.NewImageFromFile("../images/background/level3.png")
-	round1, _, _ := ebitenutil.NewImageFromFile("../images/round/ROUND1.png")
-	round2, _, _ := ebitenutil.NewImageFromFile("../images/round/ROUND2.png")
-	round3, _, _ := ebitenutil.NewImageFromFile("../images/round/ROUND3.png")
-	round4, _, _ := ebitenutil.NewImageFromFile("../images/round/ROUND4.png")
-	round5, _, _ := ebitenutil.NewImageFromFile("../images/round/ROUND5.png")
+	round1, _, _ = ebitenutil.NewImageFromFile("../images/round/ROUND1.png")
+	round2, _, _ = ebitenutil.NewImageFromFile("../images/round/ROUND2.png")
+	round3, _, _ = ebitenutil.NewImageFromFile("../images/round/ROUND3.png")
+	round4, _, _ = ebitenutil.NewImageFromFile("../images/round/ROUND4.png")
+	round5, _, _ = ebitenutil.NewImageFromFile("../images/round/ROUND5.png")
 
 	// boutons
 	playBtn, _, _ = ebitenutil.NewImageFromFile("../images/asset/play.png")
